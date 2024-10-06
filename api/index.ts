f1a08193f6bbba7ec0d84aef7a30fdca5a5cf17c
Dominik Výtisk <dominikvyt@gmail.com>
2024-10-06 15:12:46 +0200

    update

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,13 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bookingRoutes from './routes/bookingRoutes';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.get("/", (req: Request, res: Response) => {
+    res.send("Express on Vercel");
+  });
 app.use('/api', bookingRoutes);
 
 app.listen(port, () => {
